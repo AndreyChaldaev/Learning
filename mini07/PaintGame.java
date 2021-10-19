cac07e02fc3b5bdca3b3fb21c6bb89c499308da7
AndreyChaldaev <mig01@inbox.ru>
2021-10-19 22:13:18 +0500

    add 19/10

---
+++ mini07/PaintGame.java
@@ -0,0 +1,34 @@
+package com.javarush.games.minigames.mini07;
+
+import com.javarush.engine.cell.Color;
+import com.javarush.engine.cell.Game;
+
+/* 
+Работа с мышью
+*/
+
+public class PaintGame extends Game {
+
+    @Override
+    public void initialize()
+    {
+        setScreenSize(50,50);
+        for (int x = 0; x < 50; x++)
+        {
+            for (int y = 0; y < 50; y++)
+            {
+                setCellColor(x, y, Color.WHITE);
+            }
+        }
+    }
+    @Override
+    public void onMouseLeftClick (int x, int y)
+    {
+        setCellColor(x, y, Color.GREEN);
+    }
+    @Override
+    public void onMouseRightClick (int x, int y)
+    {
+        setCellColor(x, y, Color.ORANGE);
+    }
+}
